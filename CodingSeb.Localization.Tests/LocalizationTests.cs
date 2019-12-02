5a4cd79afeb1b2bd44dba6fbe9ccab2f5c253f60
Sébastien Geiser <sgeiser@precitrame.com>
2019-12-02 11:47:56 +0100

    Tests in progress

--- CodingSeb.Localization.Tests/LocalizationTests.cs
+++ CodingSeb.Localization.Tests/LocalizationTests.cs
@@ -20,7 +20,7 @@ namespace CodingSeb.Localization.Tests
 
         public string GetEmbeddedResource(string res)
         {
-            using (var reader = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream(string.Format("Localization.Tests.Resources.{0}", res))))
+            using (var reader = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream(string.Format("CodingSeb.Localization.Tests.Resources.{0}", res))))
             {
                 return reader.ReadToEnd();
             }
--- CodingSeb.Localization.WPF.Tests/LocalizationWPFTests.cs
+++ CodingSeb.Localization.WPF.Tests/LocalizationWPFTests.cs
@@ -6,6 +6,7 @@ using System.Windows.Controls;
 namespace CodingSeb.Localization.WPF.Tests
 {
     [NonParallelizable]
+    [SingleThreaded]
     public class LocalizationWPFTests
     {
         private LocalizationLoader loader;
