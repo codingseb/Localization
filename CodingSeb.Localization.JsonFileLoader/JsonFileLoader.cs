47d40d1bc76022d187fb8081afe14c9c950c2c98
Sébastien Geiser <sgeiser@precitrame.com>
2019-12-02 10:53:26 +0100

    Add JsonLoader

---
+++ CodingSeb.Localization.JsonFileLoader/JsonFileLoader.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CodingSeb.Localization.Loaders
+{
+    public class JsonFileLoader : ILocalizationFileLoader
+    {
+        /// <summary>
+        /// The separator used to concat the path of the label when structured translations are used
+        /// By default "."
+        /// </summary>
+        public string LabelPathSeparator { get; set; } = ".";
+
+        /// <summary>
+        /// The root (prefix) used for all labels
+        /// By default string.Empty
+        /// </summary>
+        public string LabelPathRootPrefix { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The leaves (suffix) used for all labels
+        /// By default string.Empty
+        /// </summary>
+        public string LabelPathSuffix { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Test sif the specified file is loadable by this Loader
+        /// </summary>
+        /// <param name="fileName">The filename to test</param>
+        /// <returns><c>true</c> if it can load the file, <c>false</c> otherwise</returns>
+        public bool CanLoadFile(string fileName)
+        {
+            return fileName.TrimEnd().EndsWith(".loc.json", StringComparison.OrdinalIgnoreCase);
+        }
+
+        /// <summary>
+        /// Load all translation defined in Json format in the specified file
+        /// </summary>
+        /// <param name="fileName">The file we want to load</param>
+        /// <param name="loader">The loader to load each translation</param>
+        public void LoadFile(string fileName, LocalizationLoader loader)
+        {
+            using(StreamReader reader = File.OpenText(fileName))
+            {
+                JObject root = (JObject)JToken.ReadFrom(new JsonTextReader(reader));
+
+                root.Properties().ToList()
+                    .ForEach(property => ParseSubElement(property, new Stack<string>(), loader, fileName));
+            }
+        }
+
+        private void ParseSubElement(JProperty property, Stack<string> textId, LocalizationLoader loader, string fileName)
+        {
+            switch(property.Value.Type)
+            {
+                case JTokenType.Object:
+                    textId.Push(property.Name);
+                    ((JObject)property.Value).Properties().ToList()
+                        .ForEach(subProperty => ParseSubElement(subProperty, textId, loader, fileName));
+                    textId.Pop();
+                    break;
+                case JTokenType.String:
+                    loader.AddTranslation(LabelPathRootPrefix + string.Join(LabelPathSeparator, textId.Reverse()) + LabelPathSuffix, property.Name, property.Value.ToString(), fileName);
+                    break;
+                default:
+                    throw new FormatException($"Invalid format in Json language file for property [{property.Name}]");
+            }
+        }
+    }
+}
---
+++ CodingSeb.Localization.JsonFileLoader/JsonMissingTranlsationsLogger.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Reflection;
+
+namespace CodingSeb.Localization
+{
+    public static class JsonMissingTranslationsLogger
+    {
+        public static void EnableLogFor(Loc loc)
+        {
+            loc.LogOutMissingTranslations = true;
+            loc.MissingTranslationFound += Loc_MissingTranslationFound;
+        }
+
+        public static void DisableLogFor(Loc loc)
+        {
+            loc.MissingTranslationFound -= Loc_MissingTranslationFound;
+        }
+
+        public static string MissingTranslationsFileName { get; set; } = Path.Combine(
+            Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+            "LocalizationMissingTranslations.json");
+
+        private static void Loc_MissingTranslationFound(object sender, LocalizationMissingTranslationEventArgs e)
+        {
+            File.WriteAllText(MissingTranslationsFileName,
+                JsonConvert.SerializeObject(e.MissingTranslations, Formatting.Indented));
+        }
+    }
+}
