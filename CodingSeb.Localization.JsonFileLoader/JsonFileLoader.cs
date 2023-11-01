d8616c5f067a4cbfdc32cd0a9e6ef4df8c5d700f
radj307 <radj307@gmail.com>
2023-11-01 15:32:47 -0400

    add support for loading from serialized strings
    - added LoadFromString to JsonFileLoader
    - added LoadFromString to YamlFileLoader
    - removed LoadStream method pushed from github
    - fixed comments in YamlFileLoader referring to Json format

--- CodingSeb.Localization.JsonFileLoader/JsonFileLoader.cs
+++ CodingSeb.Localization.JsonFileLoader/JsonFileLoader.cs
@@ -54,58 +54,61 @@ namespace CodingSeb.Localization.Loaders
         /// <param name="loader">The loader to load each translation</param>
         public void LoadFile(string fileName, LocalizationLoader loader)
         {
-            LoadStream(File.Open(fileName), loader);
+            LoadFromString(File.ReadAllText(fileName), loader, fileName);
         }
 
-        public void LoadStream(Stream stream, LocalizationLoader loader)
+        /// <summary>
+        /// Load all translations defined in Json format from the specified <paramref name="jsonString"/>.
+        /// </summary>
+        /// <param name="jsonString">String to load serialized Json format translations from.</param>
+        /// <param name="loader">The loader to use for loading translations from the string.</param>
+        /// <param name="sourceFileName">Optional source file name.</param>
+        public void LoadFromString(string jsonString, LocalizationLoader loader, string sourceFileName = "")
         {
-            using (StreamReader reader = File.OpenText(fileName))
-            {
-                JObject root = (JObject)JToken.ReadFrom(new JsonTextReader(reader));
+            JObject root = (JObject)JsonConvert.DeserializeObject(jsonString);
 
-                root.Properties().ToList()
-                    .ForEach(property => ParseSubElement(property, new Stack<string>(), loader, fileName));
-            }
+            root.Properties().ToList()
+                .ForEach(property => ParseSubElement(property, new Stack<string>(), loader, sourceFileName));
         }
 
-        private void ParseSubElement(JProperty property, Stack<string> textId, LocalizationLoader loader, string fileName)
+        private void ParseSubElement(JProperty property, Stack<string> textId, LocalizationLoader loader, string source)
         {
             switch (property.Value.Type)
             {
-                case JTokenType.Object:
-                    textId.Push(property.Name);
-                    ((JObject)property.Value).Properties().ToList()
-                        .ForEach(subProperty => ParseSubElement(subProperty, textId, loader, fileName));
-                    textId.Pop();
-                    break;
-                case JTokenType.String:
+            case JTokenType.Object:
+                textId.Push(property.Name);
+                ((JObject)property.Value).Properties().ToList()
+                    .ForEach(subProperty => ParseSubElement(subProperty, textId, loader, source));
+                textId.Pop();
+                break;
+            case JTokenType.String:
 
-                    if (LangIdDecoding == JsonFileLoaderLangIdDecoding.InFileNameBeforeExtension)
-                    {
-                        textId.Push(property.Name);
-                        loader.AddTranslation(
-                            LabelPathRootPrefix + string.Join(LabelPathSeparator, textId.Reverse()) + LabelPathSuffix,
-                            Path.GetExtension(Regex.Replace(fileName, @"\.loc\.json", "")).Replace(".", ""),
-                            property.Value.ToString(),
-                            fileName);
-                        textId.Pop();
-                    }
-                    else if (LangIdDecoding == JsonFileLoaderLangIdDecoding.DirectoryName)
-                    {
-                        textId.Push(property.Name);
-                        loader.AddTranslation(LabelPathRootPrefix + string.Join(LabelPathSeparator, textId.Reverse()) + LabelPathSuffix,
-                            Path.GetDirectoryName(fileName),
-                            property.Value.ToString(),
-                            fileName);
-                        textId.Pop();
-                    }
-                    else
-                    {
-                        loader.AddTranslation(LabelPathRootPrefix + string.Join(LabelPathSeparator, textId.Reverse()) + LabelPathSuffix, property.Name, property.Value.ToString(), fileName);
-                    }
-                    break;
-                default:
-                    throw new FormatException($"Invalid format in Json language file for property [{property.Name}]");
+                if (LangIdDecoding == JsonFileLoaderLangIdDecoding.InFileNameBeforeExtension)
+                {
+                    textId.Push(property.Name);
+                    loader.AddTranslation(
+                        LabelPathRootPrefix + string.Join(LabelPathSeparator, textId.Reverse()) + LabelPathSuffix,
+                        Path.GetExtension(Regex.Replace(source, @"\.loc\.json", "")).Replace(".", ""),
+                        property.Value.ToString(),
+                        source);
+                    textId.Pop();
+                }
+                else if (LangIdDecoding == JsonFileLoaderLangIdDecoding.DirectoryName)
+                {
+                    textId.Push(property.Name);
+                    loader.AddTranslation(LabelPathRootPrefix + string.Join(LabelPathSeparator, textId.Reverse()) + LabelPathSuffix,
+                        Path.GetDirectoryName(source),
+                        property.Value.ToString(),
+                        source);
+                    textId.Pop();
+                }
+                else
+                {
+                    loader.AddTranslation(LabelPathRootPrefix + string.Join(LabelPathSeparator, textId.Reverse()) + LabelPathSuffix, property.Name, property.Value.ToString(), source);
+                }
+                break;
+            default:
+                throw new FormatException($"Invalid format in Json language file for property [{property.Name}]");
             }
         }
     }
