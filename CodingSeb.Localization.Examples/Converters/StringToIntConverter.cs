ccbf2bb2154b594c5a0a05805c080d06a3c6c207
Sébastien Geiser <sgeiser@precitrame.com>
2024-06-12 16:08:25 +0200

    Add exemple for pluralize formatter

---
+++ CodingSeb.Localization.Examples/Converters/StringToIntConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace CodingSeb.Localization.Examples
+{
+    /// <summary>
+    /// -- Describe here to what is this class used for. (What is it's purpose) --
+    /// </summary>
+    public class StringToIntConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                return int.Parse(value.ToString());
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString();
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
